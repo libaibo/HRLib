f9822fc9a873e2f37615d5b172faea9666b5a90c
libaibo <houdian87@163.com>
2016-08-29 15:48:50 +0800

    math tool

---
+++ HRLib.framework/Headers/HRLib.h
@@ -0,0 +1,21 @@
+//
+//  HRLib.h
+//  HRLib
+//
+//  Created by li on 16/8/19.
+//  Copyright © 2016年 li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HRLib.
+FOUNDATION_EXPORT double HRLibVersionNumber;
+
+//! Project version string for HRLib.
+FOUNDATION_EXPORT const unsigned char HRLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HRLib/PublicHeader.h>
+
+
+
+#import "HRMath.h"
